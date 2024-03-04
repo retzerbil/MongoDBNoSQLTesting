3e844963839a134ba47e1f56d1f9af293dac4d3a
Andreas Retzius <andreasretzius@gmail.com>
2024-03-04 10:50:33 +0100

    Initial commit, Testing noSQL and mongodb

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://superAdmin:"+process.env.mongoDBPassword+"@project0.plqrcak.mongodb.net/?retryWrites=true&w=majority&appName=Project0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const call = await client.db("NodeJSTest").collection("Player")
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    await call.insertOne({
+        username:"Andreas",
+        email:"hejhopp",
+        password:"123qwerty"
+    })
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
